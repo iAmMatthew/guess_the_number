42daa6e1030d7708aab604565fbc495256fabc90
iAmMatthew <matthew.chi0416@icloud.com>
2014-03-19 17:59:14 +0000

    We did stuff

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 puts "Welcome to guess the number!"
-secret_number = rand(9876543210)
-puts secret_number
+secret_number = rand(500)
+
+puts "Geuss a number between 0 and 500:"
+guess = gets.chomp.to_i
+
+until guess == secret_number
+puts "Wrong! Try again."
+    guess = gets.chomp.to_i
+end
+
+puts "Congratz! You is right!"
