96279702ae175839428f449f2c3cb3ed00fafda3
iAmMatthew <matthew.chi0416@icloud.com>
2014-03-20 17:48:42 +0000

    Added a hint to tell if number is over 50 or under 50.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,12 +1,19 @@
 puts "Welcome to guess the number!"
-secret_number = rand(500)
+secret_number = rand(101)
 
-puts "Geuss a number between 0 and 500:"
+puts "Guess number 0 to 100."
+if secret_number < 50
+    puts "The number is small yafeel."
+else
+  puts "The number is big yafeel."
+end
 guess = gets.chomp.to_i
 
 until guess == secret_number
 puts "Wrong! Try again."
     guess = gets.chomp.to_i
 end
+
+
 
 puts "Congratz! You is right!"
